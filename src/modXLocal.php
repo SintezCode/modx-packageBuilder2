465bc2b0b7ff98b767a0a250c643c7c0fb3ad493
SintezCode <unarmedf@gmail.com>
2021-02-26 16:09:50 +0300

    Add modXLocal class

---
+++ src/modXLocal.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SintezCode;
+
+use modX;
+
+/**
+ * This is the class extends modX for locally building packages.
+ *
+ *
+ */
+class modXLocal extends modX
+{
+
+}
