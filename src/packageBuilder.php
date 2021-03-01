d1e422a828e7b36d680fc25311da0d16dd2e043a
SintezCode <unarmedf@gmail.com>
2021-03-01 16:33:06 +0300

    Remove previous version code

--- src/packageBuilder.php
+++ src/packageBuilder.php
@@ -24,9 +24,7 @@ class packageBuilder{
     public function __construct(modX &$modx, array $config=[]){
         $this->modx=$modx;
         $this->config=$config;
-        $this->config['component']['modelPath']=$this->config['component']['modelPath']?:($this->config['component']['core'].'model/');
-        $this->config['component']['schemaPath']=$this->config['component']['schemaPath']?:($this->config['component']['modelPath'].'schema/');
-        $this->config['component']['servicePath']=$this->config['component']['servicePath']?:($this->config['component']['modelPath'].$this->config['component']['namespace'].'/');
+
 
         $this->modx->loadClass('transport.modPackageBuilder','',false, true);
     }
